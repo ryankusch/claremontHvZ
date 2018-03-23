02729d2e9f2c25f9251f013563252157de3e3cd6
Mohammad Bakhiet <mbakhiet@g.hmc.edu>
2018-03-23 10:28:32 -0700

    display names and emails

--- HVZ/HVZ/api/views.py
+++ HVZ/HVZ/api/views.py
@@ -1,24 +1,21 @@
 import json
 
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpRequest
 from HVZ.main.models import Player
 
-def json_get_all_graduation_years(request):
-    """A pretty useless function that displays all graduation years.
-
-    You should replace this with one you actually want.
-
+def json_get_all_emails(request):
+    """A function that displays every players email
+    in the current game.
     """
 
     # Check out HVZ/main/models.py for helper functions relating to Players.
     # Player.current_players() returns all Players in the current Game.
-    years = [p.grad_year for p in Player.current_players()]
+    emails = [(str(p.user.first_name) + " " + str(p.user.last_name), p.mailbox) for p in Player.current_players()]
 
     # json.dumps creates a string from a Python object. You can then
     # read the string and convert it into an Objective-C data
     # structure using NSJSONSerialization in Objective-C.
-    json_data = json.dumps(years)
+    json_data = json.dumps(emails)
 
     return HttpResponse(
         json_data,
